0221041fbe8d70731b5a4ee01508e8091980bbb0
Mohamed Abdelsatar <m_abdelsatarjr@yahoo.com>
2025-01-11 02:15:03 +0200

    Modeling2

--- dags/modeling.py
+++ dags/modeling.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.dbt.cloud.operators.dbt import DbtRunOperator
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
 from datetime import datetime
 
 with DAG(
@@ -9,37 +10,37 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    run_fact_orders = DbtRunOperator(
+    run_fact_orders = DbtCloudRunJobOperator(
         task_id="run_fact_orders",
         models="Olist.olist_satar_fact"  
     )
 
-    run_dim_top_customers = DbtRunOperator(
+    run_dim_top_customers = DbtCloudRunJobOperator(
         task_id="run_dim_top_customers",
         models="Olist.olist_satar_cust",
     )
 
-    run_dim_top_products = DbtRunOperator(
+    run_dim_top_products = DbtCloudRunJobOperator(
         task_id="run_dim_top_products",
         models="Olist.olist_satar_prod",
     )
 
-    run_dim_monthly_orders = DbtRunOperator(
+    run_dim_monthly_orders = DbtCloudRunJobOperator(
         task_id="run_dim_monthly_orders",
         models="Olist.olist_satar_ord",
     )
 
-    run_dim_payment_distribution = DbtRunOperator(
+    run_dim_payment_distribution = DbtCloudRunJobOperator(
         task_id="run_dim_payment_distribution",
         models="Olist.olist_satar_pay",
     )
 
-    run_dim_avg_orders_per_customer = DbtRunOperator(
+    run_dim_avg_orders_per_customer = DbtCloudRunJobOperator(
         task_id="run_dim_avg_orders_per_customer",
         models="Olist.olist_satar_ord_cast",
     )
 
-    run_dim_category_revenue = DbtRunOperator(
+    run_dim_category_revenue = DbtCloudRunJobOperator(
         task_id="run_dim_category_revenue",
         models="Olist.olist_satar_cat",
     )
